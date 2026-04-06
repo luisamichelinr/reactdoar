316911d4ffe3651a44bffec66bed32d1d8952a75
caiquefgoncalves <caiquefachinigoncalves@gmail.com>
2026-04-06 18:49:15 -0300

    Atualizando
    Refactor Select component to accept value and onChange props.

--- src/components/Select/Select.jsx
+++ src/components/Select/Select.jsx
@@ -1,17 +1,16 @@
 import css from './Select.module.css'
-import { useState } from 'react'
 
-export default function Select({options = [], label, required = false }) {
-    const [valor, setValor] = useState('')
-
-    function alterar(e) {
-        setValor(e.currentTarget.value)
-    }
-
+export default function Select({ options = [], label, required = false, value = '', onChange = null }) {
     return (
         <div className={css.selectGroup}>
             <label className={css.titulo}>{label}</label>
-            <select className={css.select1} onChange={alterar} value={valor} required={required}>
+            <select 
+                className={css.select1} 
+                onChange={onChange} 
+                value={value} 
+                required={required}
+            >
+                <option value="" disabled>Selecione uma opção</option>
                 {options.map((opt, i) => (
                     <option key={i} value={opt}>
                         {opt}
