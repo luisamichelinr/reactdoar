da8e6056da85c752e2991e1cbb77a6d62f8b6741
Luísa Michelin Riqueti <luisa.michelinr@gmail.com>
2026-04-07 14:44:39 -0300

    Upload

--- src/components/AcessoRestrito1/AcessoRestrito1.jsx
+++ src/components/AcessoRestrito1/AcessoRestrito1.jsx
@@ -9,14 +9,11 @@ export default function AcessoRestrito1() {
     const navigate = useNavigate();
     const nome = localStorage.getItem('nome');
     const [error, setError] = useState('');
-    const sucesso = localStorage.getItem('sucesso');
-    if (sucesso) {
-        localStorage.removeItem('sucesso');
-    }
+    const [sucesso, setSucesso] = useState(localStorage.getItem('sucesso')); // Usar estado
 
     async function Logout() {
         const token = localStorage.getItem('token');
-        await fetch('http://10.92.3.125:5000/logout', {
+        await fetch('http://10.92.3.146:5000/logout', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -26,23 +23,42 @@ export default function AcessoRestrito1() {
 
         localStorage.removeItem("token");
         localStorage.removeItem("nome");
+        localStorage.setItem('sucesso', 'Usuário deslogado com sucesso!');
 
         navigate("/login");
     }
 
     useEffect(function () {
         const token = localStorage.getItem('token');
-
         if (!token) {
             navigate("/login");
         }
     }, [])
 
+
+    const fecharMensagemSucesso = () => {
+        localStorage.removeItem('sucesso');
+        setSucesso(null);
+    }
+
+
+    const fecharMensagemErro = () => {
+        setError('');
+    }
+
     return (
         <div className={css.inicio}>
             <div>
-                <Mensagem tipo={"sucesso"} texto={sucesso} onClose={() => setError('')}/>
-                <Mensagem tipo={"erro"} texto={error} onClose={() => setError('')}/>
+                <Mensagem
+                    tipo={"sucesso"}
+                    texto={sucesso}
+                    onClose={fecharMensagemSucesso}
+                />
+                <Mensagem
+                    tipo={"erro"}
+                    texto={error}
+                    onClose={fecharMensagemErro}
+                />
             </div>
             <Titulo titulo={'Olá, ' + nome} cor={'azul-claro'} texto={'Essa página é restrita, sendo somente acessada com validação'}/>
             <div>
